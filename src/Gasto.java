69fecd5d3c7b206d97813d060ea3b6cbabf2c829
Samuel Ribas <samuelribas2233@gmail.com>
2025-09-27 12:05:30 -0300

    First Commit Gasto.java
    Teste 01

--- src/Gasto.java
+++ src/Gasto.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Gasto {
+    private String categoria;
+    private double valor;
+    private String data;
+    
+    //Getters
+    public String getGasto() {
+
+        String exibir ="Categoria: " + categoria +
+                "\nValor: R$" + String.format("%.2f", valor) +
+                "\nData: " + data + "\n--------------\n";
+        return exibir;
+    }
+    public String getCategoria() { return categoria; }
+
+    public double getValor() { return valor; }
+
+    public String getData() {
+        return data;
+    }
+    // Setters
+    public void setCategoria(String categoria) {
+
+        this.categoria = categoria;
+
+    }
+
+    public void setValor(double valor) {
+
+        this.valor = valor;
+    }
+
+    public void setData(String data) {
+
+        this.data = data;
+    }
+
 }
