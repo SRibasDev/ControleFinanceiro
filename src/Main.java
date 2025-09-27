b3e23db9b2a0406b8aa933507a85beddd54c0bb2
Samuel Ribas <samuelribas2233@gmail.com>
2025-09-27 12:06:06 -0300

    Update Main.java
    Teste

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        Gasto conta = new Gasto();
+        ArrayList<Gasto> vetorGastos = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int num;
+        do {
+            System.out.println("======================");
+            System.out.println("1 - Adicionar gasto");
+            System.out.println("2 - Remover gasto ");
+            System.out.println("3 - Exibir dados");
+            System.out.println("4 - Sair");
+            System.out.println("======================");
+            num = input.nextInt();
+            input.nextLine(); // Limpa buffer
+
+            switch (num) {
+                case 1:
+                    System.out.println("Categoria: ");
+                    String categoria = input.nextLine();
+
+                    System.out.println("Valor: ");
+                    double valor = input.nextDouble();
+                    input.nextLine(); // Limpa buffer
+
+                    System.out.println("Data: ");
+                    String data = input.nextLine();
+
+                    // Cria e adiciona um objeto no array
+                    Gasto novoGasto = new Gasto();
+                    novoGasto.setCategoria(categoria);
+                    novoGasto.setValor(valor);
+                    novoGasto.setData(data);
+                    vetorGastos.add(novoGasto);
+
+                    System.out.println("Adicionado com sucesso!");
+                    break;
+
+                case 2:
+                    System.out.println("Removendo gastos...");
+                    break;
+
+                case 3:
+                    System.out.println("===================");
+                    System.out.println("====  Gastos  ====");
+                    if (vetorGastos.isEmpty()) {
+                        System.out.println("Nenhum gasto cadastrado.");
+                        continue;
+                    } else {
+                        for (Gasto xpto : vetorGastos) {
+                            System.out.println(xpto.getGasto());
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+            }
+        } while (num != 4);
     }
 }
